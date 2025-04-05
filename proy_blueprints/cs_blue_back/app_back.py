e0975267ad864ad143b49c5bb01b2756510fc3b1
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-04-05 08:08:21 -0500

    Quiitando front innecesario

--- proy_blueprints/cs_blue_back/app_back.py
+++ proy_blueprints/cs_blue_back/app_back.py
@@ -14,4 +14,4 @@ app.register_blueprint(autor_bp, url_prefix="/api/autores")
 app.register_blueprint(libro_bp, url_prefix="/api/libros")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
